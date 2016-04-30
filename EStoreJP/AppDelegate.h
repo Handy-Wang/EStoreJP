e0e8315f9fbf60805d26cdd33212b291e4b7d525
jiangshan.wjs <jiangshan.wjs@alibaba-inc.com>
2016-05-01 02:40:50 +0800

    [EStoreJP - init] Push proj initialisation.

---
+++ EStoreJP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EStoreJP
+//
+//  Created by Handy on 5/1/16.
+//  Copyright © 2016 Handy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EStoreJP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EStoreJP
+//
+//  Created by Handy on 5/1/16.
+//  Copyright © 2016 Handy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
